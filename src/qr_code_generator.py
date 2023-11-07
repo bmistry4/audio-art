614f128621d06608be14e181825a7e5412f78b82
bmistry4 <mistrybhk@gmail.com>
2023-11-07 14:21:03 +0000

    clean up qrcode for blog use

--- src/qr_code_generator.py
+++ src/qr_code_generator.py
@@ -15,26 +15,38 @@ from utils.file_loading import create_directory
 
 ########################################################################################################################
 class RunID(Enum):
-    F1 = "bkgThickFloral_logoFloral"
-    F2 = "bkgThickFloral_logoFloralRound"
+    F0 = "bkgNone_logoNone"
+    F1 = "bkgNone_logoFloralBorderAndCenter"
+    F2 = "bkgNone_logoFloralCenter"
     F3 = "bkgFloralBorderAndCenter_logoNone"
-    F4 = "bkgThickFloral_logoNone"
-    F5 = "bkgThickFloral_logoWaves"
+    F4 = "bkgFloralThickBorderAndCenter_logoNone"
+    F5 = "bkgFloralBorder_logoNone"
+    F6 = "bkgFloralThickBorder_logoNone"
+    F7 = "bkgFloralThickBorder_logoFloralCenter"
+    F8 = "bkgThickFloralBorder_logoWaves"
 
-# TODO: you'll need to copy over the relevant files to the ../images/qrcode/load
+
+# TODO: you'll need to copy over the relevant files to the ../images/qrcode/blog/load
 id_to_meta_files = {
+    RunID.F0: {
+        "background": None,
+        "logo": None,
+        "round_logo": False,
+        "logo_radius": 2000,
+    },
+
     RunID.F1: {
-        "background": "final-polar-floral-thickBorder_dpi-600",
-        "logo": "final-polar-floral-center_dpi-600",
+        "background": None,
+        "logo": "final-polar-floral-borderAndCenter_dpi-600",
         "round_logo": True,
         "logo_radius": 2000,
     },
 
     RunID.F2: {
-        "background": "final-polar-floral-thickBorder_dpi-600",
-        "logo": "final-polar-floral-center-round_dpi-600",
+        "background": None,
+        "logo": "final-polar-floral-center_dpi-600",
         "round_logo": True,
-        "logo_radius": 1400,
+        "logo_radius": 2000,
     },
 
     RunID.F3: {
@@ -45,18 +57,40 @@ id_to_meta_files = {
     },
 
     RunID.F4: {
-        "background": "final-polar-floral-thickBorder_dpi-600",
+        "background": "final-polar-floral-thickBorderAndCenter_dpi-600",
         "logo": None,
         "round_logo": False,
         "logo_radius": 2000,
     },
 
     RunID.F5: {
+        "background": "final-polar-floral-border_dpi-600",
+        "logo": None,
+        "round_logo": False,
+        "logo_radius": 2000,
+    },
+
+    RunID.F6: {
+        "background": "final-polar-floral-thickBorder_dpi-600",
+        "logo": None,
+        "round_logo": False,
+        "logo_radius": 2000,
+    },
+
+    RunID.F7: {
+        "background": "final-polar-floral-thickBorder_dpi-600",
+        "logo": "final-polar-floral-center_dpi-600",
+        "round_logo": True,
+        "logo_radius": 2000,
+    },
+
+    RunID.F8: {
         "background": "final-polar-floral-thickBorder_dpi-600",
         "logo": "final-frequency-waves-overlapping_dpi-600",
         "round_logo": True,
         "logo_radius": 2000,
     },
+
 }
 
 
@@ -68,7 +102,7 @@ def add_suffix_to_filepath(filename, suffix):
 
 
 def add_corners(im, rad):
-    # https://github.com/reegan-anne/python_qrcode/blob/main/main.ipynb
+    # round an image. Taken from https://github.com/reegan-anne/python_qrcode/blob/main/main.ipynb
     circle = Image.new('L', (rad * 2, rad * 2), 0)
     draw = ImageDraw.Draw(circle)
     draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
@@ -85,22 +119,24 @@ def add_corners(im, rad):
 def create_savepath(dir, filename, module_drawer, dpi, ext):
     return os.path.join(dir, f"qrcode_{filename}_dpi-{dpi}_{module_drawer}{ext}")
 
+########################################################################################################################
+# TODO - SET PARAMETERS AS REQUIRED
+data = sys.argv[1]  # whatever you want the qrcode to link to
+SAVE = True
+dpi = 600  # set dpi for higher resolution - important if you're gonna canvas print
+box_size = 30  # higher box size = better quality image; use 50 for canvas print
+ext = ".png"
+
+load_dir = "../images/qrcode/blog/load/"
+save_dir = "../images/qrcode/blog/save/"
+create_directory(load_dir)
+create_directory(save_dir)
 
 ########################################################################################################################
-# for id in [RunID.F2]:
+# for id in [RunID.F0]:
 for id in RunID:
-    ID = id         # canvas print ID = RunID.F2
+    ID = id
     save_name = ID.value
-    data = sys.argv[1]
-    SAVE = True
-    dpi = 600       # set dpi for higher resolution
-    box_size = 30   # higher box size = better quality image; use 50 for canvas print
-    ext = ".png"
-
-    load_dir = "../images/qrcode/load/"
-    save_dir = "../images/qrcode/save/"
-    create_directory(load_dir)
-    create_directory(save_dir)
 
     round_radius = id_to_meta_files[ID]["logo_radius"]
 
@@ -111,9 +147,6 @@ for id in RunID:
         if (id_to_meta_files[ID]["logo"] is not None) else None
 
     rounded_logo_img = add_suffix_to_filepath(logo_img, "_rounded") if id_to_meta_files[ID]["round_logo"] else None
-
-    if bkg_img is None:
-        raise Exception("Need a file for background image! having no background isn't supported!")
 
     print("bkg\t", bkg_img)
     print("logo\t", logo_img)
@@ -138,10 +171,16 @@ for id in RunID:
                           SquareModuleDrawer, HorizontalBarsDrawer, GappedSquareModuleDrawer]:
         qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, border=6, box_size=box_size)
         qr.add_data(data)
+
+        # set background for qr code - will either be plain white or an image
+        qr_img_colour_mask = {}
+        if bkg_img:
+            qr_img_colour_mask = {"color_mask": ImageColorMask(back_color=(1, 1, 1), color_mask_path=bkg_img)}
+
         qr_img = qr.make_image(image_factory=StyledPilImage,
                                module_drawer=module_drawer(),
                                embeded_image_path=logo_img,
-                               color_mask=ImageColorMask(back_color=(1, 1, 1), color_mask_path=bkg_img),
+                               **qr_img_colour_mask
                                )
 
         if SAVE:
@@ -149,6 +188,5 @@ for id in RunID:
             qr_img.save(save_path, dpi=(dpi, dpi))
             print(f"saved at: {save_path}")
 
-
 print("completed")
-# qr_img.show()
+qr_img.show()
--- src/utils/image_joiner.py
+++ src/utils/image_joiner.py
@@ -15,7 +15,13 @@ id_to_filenames = {
     2: ['2-radii-offset-0', '2-radii-offset-1'],
     3: ['3-binsNum-1', '3-binsNum-2', '3-binsNum-5', '3-binsNum-10', '3-binsNum-15', '3-binsNum-20'],
     4: ['4-binsSize-50', '4-binsSize-100', '4-binsSize-200', '4-binsSize-300', '4-binsSize-400', '4-binsSize-500'],
-    5: ['5-colours-0', '5-colours-1', '5-colours-2', '5-colours-3']
+    5: ['5-colours-0', '5-colours-1', '5-colours-2', '5-colours-3'],
+    6: ['qrcode_bkgThickFloralBorder_logoWaves_dpi-600_CircleModuleDrawer',
+        'qrcode_bkgThickFloralBorder_logoWaves_dpi-600_GappedSquareModuleDrawer',
+        'qrcode_bkgThickFloralBorder_logoWaves_dpi-600_HorizontalBarsDrawer',
+        'qrcode_bkgThickFloralBorder_logoWaves_dpi-600_RoundedModuleDrawer',
+        'qrcode_bkgThickFloralBorder_logoWaves_dpi-600_SquareModuleDrawer',
+        'qrcode_bkgThickFloralBorder_logoWaves_dpi-600_VerticalBarsDrawer']
 }
 
 id_to_subplot_titles = {
@@ -23,7 +29,9 @@ id_to_subplot_titles = {
     2: ['radii-offset = 0', 'radii-offset = 1'],
     3: ['No. bins = 1', 'No. bins = 2', 'No. bins = 5', 'No. bins = 10', 'No. bins = 15', 'No. bins = 20'],
     4: ['Bin size = 50', 'Bin size = 100', 'Bin size = 200', 'Bin size = 300', 'Bin size = 400', 'Bin size = 500'],
-    5: ['', '', '', '']
+    5: ['', '', '', ''],
+    6: ['CircleModuleDrawer', 'GappedSquareModuleDrawer', 'HorizontalBarsDrawer', 'RoundedModuleDrawer',
+        'SquareModuleDrawer', 'VerticalBarsDrawer']
 }
 
 id_to_savename = {
@@ -32,6 +40,7 @@ id_to_savename = {
     3: "3-binsNum",
     4: "4-binSize",
     5: "5-colours",
+    6: "qrcode_bkgThickFloralBorder_logoWaves_dpi-600-ModuleDrawers"
 }
 
 id_to_grid_size = {
@@ -39,7 +48,9 @@ id_to_grid_size = {
     2: (1, 2),
     3: (2, 3),
     4: (2, 3),
-    5: (2, 2)
+    5: (2, 2),
+    6: (2, 3),
+
 }
 
 
